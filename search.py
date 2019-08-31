d98555db46b48de2901cef108563270583a20b20
Lucas Dalle <38118199+xDalle@users.noreply.github.com>
2019-08-31 13:51:24 -0300

    Add files via upload

--- search.py
+++ search.py
@@ -23,7 +23,6 @@ class SearchProblem:
     """
     This class outlines the structure of a search problem, but doesn't implement
     any of the methods (in object-oriented terminology: an abstract class).
-
     You do not need to change anything in this class, ever.
     """
 
@@ -36,7 +35,6 @@ class SearchProblem:
     def isGoalState(self, state):
         """
           state: Search state
-
         Returns True if and only if the state is a valid goal state.
         """
         util.raiseNotDefined()
@@ -44,7 +42,6 @@ class SearchProblem:
     def getSuccessors(self, state):
         """
           state: Search state
-
         For a given state, this should return a list of triples, (successor,
         action, stepCost), where 'successor' is a successor to the current
         state, 'action' is the action required to get there, and 'stepCost' is
@@ -55,7 +52,6 @@ class SearchProblem:
     def getCostOfActions(self, actions):
         """
          actions: A list of actions to take
-
         This method returns the total cost of a particular sequence of actions.
         The sequence must be composed of legal moves.
         """
@@ -75,13 +71,10 @@ def tinyMazeSearch(problem):
 def depthFirstSearch(problem):
     """
     Search the deepest nodes in the search tree first.
-
     Your search algorithm needs to return a list of actions that reaches the
     goal. Make sure to implement a graph search algorithm.
-
     To get started, you might want to try some of these simple commands to
     understand the search problem that is being passed in:
-
     print("Start:", problem.getStartState())
     print("Is the start a goal?", problem.isGoalState(problem.getStartState()))
     print("Start's successors:", problem.getSuccessors(problem.getStartState()))
@@ -131,7 +124,22 @@ def nullHeuristic(state, problem=None):
 def aStarSearch(problem, heuristic=nullHeuristic):
     """Search the node that has the lowest combined cost and heuristic first."""
     "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+	def djikstra(initial):
+		from util import PriorityQueue
+		q = PriorityQueue()
+		q.push((initial, []), 0)
+		dist = Dict()
+		dist[initial] = 0
+		while(not q.isEmpty()):
+			u, path = q.pop()
+			if u == problem.isGoalState():
+				return path
+			for (v, action, coust) in problem.getSuccessors(u):
+				if v not in dist or dist[v] > dist[u] + coust + heuristic(v, problem):
+					dist[v] = dist[u] + coust + heuristic(v, problem)
+					q.push((v, path + [action]), dist[v])
+	return djikstra(problem.getStartState())
+    # util.raiseNotDefined()
 
 
 # Abbreviations
